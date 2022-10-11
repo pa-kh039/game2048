5d7ba541a5d8023a44fa404439bb1e899deb2f67
parth <parthkhandenath123@gmail.com>
2022-10-11 18:32:24 +0530

    added if compression or merge lead to change

--- Logics.py
+++ Logics.py
@@ -43,23 +43,28 @@ def get_current_state(mat):
 
 def compress(mat):
     # a new matrix in which all the non-zero numbers are on one side
+    changed=False
     new_mat=[[0 for i in range(4)] for j in range(4)]
     for i in range(4):
         pos=0
         for j in range(4):
             if mat[i][j]!=0:
                 new_mat[i][pos]=mat[i][j]
+                if j!=pos:
+                    changed=True
                 pos+=1
-    return new_mat
+    return new_mat,changed
 
 def merge(mat):
     # combining the adjacent same elements
+    changed=False
     for i in range(4):
         for j in range(3):
             if mat[i][j]==mat[i][j+1] and mat[i][j]>0:
                 mat[i][j]=2*mat[i][j]
                 mat[i][j+1]=0
-    return mat
+                changed=True
+    return mat,changed
 
 def reverse(mat):
     #it reverses each row of the matrix
@@ -80,35 +85,36 @@ def transpose(mat):
 
 def move_up(mat):
     trans_mat=transpose(mat)
-    new_mat=compress(trans_mat)
-    new_mat=merge(new_mat)
+    new_mat,c1=compress(trans_mat)
+    new_mat,c2=merge(new_mat)
     new_mat=compress(new_mat)
     new_mat=transpose(new_mat)
-    return new_mat
+    return new_mat, c1 or c2
 
 def move_down(mat):
     trans_mat=transpose(mat)
     reversed_mat=reverse(trans_mat)
-    new_mat=compress(reversed_mat)
-    new_mat=merge(new_mat)
+    new_mat,c1=compress(reversed_mat)
+    new_mat,c2=merge(new_mat)
     new_mat=compress(new_mat)
     new_mat=reverse(new_mat)
     new_mat=transpose(new_mat)
-    return new_mat
+    return new_mat, c1 or c2
 
 def move_left(mat):
-    new_mat=compress(mat)
-    new_mat=merge(new_mat)
-    new_mat=compress(new_mat)
-    return new_mat
+    new_mat,c1=compress(mat)
+    new_mat,c2=merge(new_mat)
+    c=c1 or c2
+    new_mat,temp=compress(new_mat)
+    return new_mat,c
 
 def move_right(mat):
     new_mat=reverse(mat)
-    new_mat=compress(new_mat)
-    new_mat=merge(new_mat)
+    new_mat,c1=compress(new_mat)
+    new_mat,c2=merge(new_mat)
     new_mat=compress(new_mat)
     fin_mat=reverse(new_mat)
-    return fin_mat
+    return fin_mat, c1 or c2
 
 # --------------testing-------------- #
 # mat=start_game()
